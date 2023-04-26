398d9de9f1c2891256a4e2be8fe662ff9148a464
OlyaDa <dashevska.olga@gmail.com>
2023-04-26 10:01:57 +0300

    add all prj

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const selects = document.querySelectorAll('.select');
+selects.forEach(el => {
+    new Choices(el, {
+        shouldSort: false,
+        position: 'bottom',
+        searchEnabled: false,
+    });
+});
