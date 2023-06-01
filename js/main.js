754985e3028b19338cb8f01defc341f4dc391702
OlyaDa <dashevska.olga@gmail.com>
2023-06-01 15:55:05 +0300

    add history page with datarangepicker

--- js/main.js
+++ js/main.js
@@ -22,3 +22,79 @@ scrollBtn.onclick = () => {
         behavior: "smooth",
     });
 }
+
+
+// $(function () {
+//     $('input[name="datefilter"]').daterangepicker({
+//         linkedCalendars: false,
+//         ranges: {
+//             'Today': [moment(), moment()],
+//             'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+//             'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+//             'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+//             'This Month': [moment().startOf('month'), moment().endOf('month')],
+//             'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+//         },
+//         "locale": {
+//             "format": "DD/MM/YYYY",
+//         },
+//         opens: 'right',
+//         "timePicker": true,
+//     }, function (start, end, label) {
+//         console.log("A new date selection was made: " + start.format('DD-MM-YYYY') + ' to ' + end.format('DD-MM-YYYY'));
+//     });
+// });
+
+$(function () {
+    // Get the current screen width
+    var screenWidth = $(window).width();
+
+    // Define the date range picker options
+    var daterangepickerOptions = {
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [
+                moment().subtract(1, 'month').startOf('month'),
+                moment().subtract(1, 'month').endOf('month')
+            ]
+        },
+        locale: {
+            format: 'DD/MM/YYYY',
+            "monthNames": [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            ],
+        },
+        opens: 'right',
+        timePicker: true,
+        linkedCalendars: screenWidth < 900 ? false : true
+    };
+
+    // Initialize the date range picker
+    $('input[name="datefilter"]').daterangepicker(daterangepickerOptions, function (
+        start,
+        end,
+        label
+    ) {
+        console.log(
+            'A new date selection was made: ' +
+            start.format('DD-MM-YYYY') +
+            ' to ' +
+            end.format('DD-MM-YYYY')
+        );
+    });
+});
